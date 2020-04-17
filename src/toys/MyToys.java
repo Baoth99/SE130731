e71c8a3e36736f037010c07ea092f33bbf7fc46e
Baoth99 <baoth799@gmail.com>
2020-04-17 14:18:31 +0700

    6th push: green color and recursion for refactoring

--- src/toys/MyToys.java
+++ src/toys/MyToys.java
@@ -17,14 +17,17 @@ public class MyToys {
     public static long cf(int n) {
         if(n < 0 || n > 15)
             throw new IllegalArgumentException("n must between 0...15");
-        if(n == 0)
-            return 1;
-        long product = 1;
-        for (int i = 1; i <= n; i++) {
-            product *= i;
-        }
-        return product;
+//        if(n == 0)
+//            return 1;
+//        long product = 1;
+//        for (int i = 1; i <= n; i++) {
+//            product *= i;
+//        }
+//        return product;
         //bữa sau đệ quy vì kết hợp với CI Continuous
             //Integration
+            if(n == 0 || n == 1)
+                return 1;
+            return cf(n - 1) * n;
     }
 }
